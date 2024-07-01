523fe6f0cc9521e122dc1e3b4e2a8d7e836b8a56
Hirynnn <aguiarv05@gmail.com>
2024-07-01 15:54:56 -0300

    projeto completo

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia a2 = new Nokia("3232","A32","34343",45);
+a2.ReceberLigacao();
+a2.Ligar();
+a2.InstalarAplicativo("Whats");
+
+
+
+Iphone i13 = new Iphone("34345","Iphone13","233243",120);
+i13.ReceberLigacao();
+i13.Ligar();
+i13.InstalarAplicativo("Genshin Impact");
